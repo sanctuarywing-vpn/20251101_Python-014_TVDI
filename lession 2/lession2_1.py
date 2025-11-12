f3f068ac13131ab6fabcb985a00aacfb0a7fd654
sanctuarywing <sanctuarywing@gmail.com>
2025-11-12 11:38:27 +0800

    hw1108rev1

--- lession 2/lession2_1.py
+++ lession 2/lession2_1.py
@@ -1,1 +1,6 @@
 print("Hello! python!123")
+# This is a sample Python script for lesson 2_1
+## You can run this script in your Python environment to see the output.
+### Press Shift+F10 to execute the script or replace it with your own code.
+#### Use the 'Run' menu at the top of the IDE and select 'Run...' to run the script. 會自動進入虛擬環境
+##### To debug the script, press Shift+F9.
