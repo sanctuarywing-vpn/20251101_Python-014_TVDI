76a9ab8b3749a408daa46d9192fe65439db4f820
sanctuarywing <sanctuarywing@gmail.com>
2025-11-29 12:02:40 +0800

    1129 - am

---
+++ lession5/lession5_2.py
@@ -0,0 +1,22 @@
+import os
+from playwright.sync_api import sync_playwright
+
+def main():
+    path = "https://www.thsrc.com.tw/"
+    with sync_playwright() as p:
+        # 啟動瀏覽器
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+
+        # 打開新頁面
+        page = browser.new_page()
+
+        page.goto(path)
+
+        
+        page.wait_for_load_state("domcontentloaded")  # 等待網絡結構完成不等資料下載結束
+        page.wait_for_timeout(3000)  # 等待3秒以觀察效果
+
+        browser.close()
+
+if __name__ == "__main__":
+    main()
